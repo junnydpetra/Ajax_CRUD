70a24eaa0653cba3b2435b38b08b15c4bdab6871
junnydpetra <junnycmenezes@gmail.com>
2023-01-13 17:17:29 -0300

    CRUD Concluído

--- ajax/cadastrar.php
+++ ajax/cadastrar.php
@@ -3,27 +3,21 @@ include "db.php";
 $erro = array();
 $saida = "";
 
-$nome = addslashes($_POST['nome']);
-$email = addslashes($_POST['email']);
-$estado = addslashes($_POST['estado']);
-$cpf = addslashes($_POST['cpf']);
-
-if (empty($nome))
+if (isset($_POST['id'])) 
 {
-    $erro['e'] = "Informe um nome válido!";
-} else if (empty($email)) {
-    $erro['e'] = "Informe um email válido!";
-} else if (empty($estado)) {
-    $erro['e'] = "Selecione um estado!";
-} else if (empty($cpf)) {
-    $erro['e'] = "Informe um CPF válido!";
-} else {
-    $sql = 'INSERT INTO user(nome, email, estado, cpf) VALUE(:n, :m, :e, :c)';
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $estado = addslashes($_POST['estado']);
+    $cpf = addslashes($_POST['cpf']);
+    $id = $_POST['id'];
+
+    $sql = 'UPDATE user SET nome = :n, email = :m, estado = :e, cpf = :c WHERE id = :id';
     $cmd = $pdo->prepare($sql);
     $cmd->bindValue(":n", $nome);
     $cmd->bindValue(":m", $email);
     $cmd->bindValue(":e", $estado);
     $cmd->bindValue(":c", $cpf);
+    $cmd->bindValue(":id", $id);
     $cmd->execute();
 
     if ($cmd->rowCount() >= 1) {
@@ -31,6 +25,38 @@ if (empty($nome))
         Usuário " .$nome. " cadastrado com sucesso!</p>";
     } else {
         $saida .= "<p class='alert alert-danger text-center mt-3'> Falha ao realizar cadastro! </p>";     
+    }
+
+} else {
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $estado = addslashes($_POST['estado']);
+    $cpf = addslashes($_POST['cpf']);
+
+    if (empty($nome))
+    {
+        $erro['e'] = "Informe um nome válido!";
+    } else if (empty($email)) {
+        $erro['e'] = "Informe um email válido!";
+    } else if (empty($estado)) {
+        $erro['e'] = "Selecione um estado!";
+    } else if (empty($cpf)) {
+        $erro['e'] = "Informe um CPF válido!";
+    } else {
+        $sql = 'INSERT INTO user(nome, email, estado, cpf) VALUE(:n, :m, :e, :c)';
+        $cmd = $pdo->prepare($sql);
+        $cmd->bindValue(":n", $nome);
+        $cmd->bindValue(":m", $email);
+        $cmd->bindValue(":e", $estado);
+        $cmd->bindValue(":c", $cpf);
+        $cmd->execute();
+
+        if ($cmd->rowCount() >= 1) {
+            $saida .= "<p class='alert alert-success text-center mt-3'> 
+            Usuário " .$nome. " cadastrado com sucesso!</p>";
+        } else {
+            $saida .= "<p class='alert alert-danger text-center mt-3'> Falha ao realizar cadastro! </p>";     
+        }
     }
 }
 
--- ajax/select.php
+++ ajax/select.php
@@ -5,28 +5,34 @@ include "db.php";
 $base = "";
 
 if (isset($_POST['id'])) {
+    $id = $_POST['id'];
     $sql = 'SELECT * FROM user WHERE id = :id';
     $cmd = $pdo->prepare($sql);
+    $cmd->bindValue(":id", $id);
     $cmd->execute();
-    $dados = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    $dados = $cmd->fetch();
 
     $base .= '
-        <form action="" class="form-group" id="form">
+        <form action="" class="form-group" id="update">
             <div class="row">
                 <div class="col-sm-3">
                     <label class="form-label">Nome</label>
-                    <input type="text" name="nome" id="nome" class="form-control" placeholder="Informe o nome"> 
+                    <input type="text" name="nome" id="nome" class="form-control" 
+                     placeholder="Informe o nome" value="'.$dados['nome'].'"> 
                 </div>
                 
                 <div class="col-sm-3">
                     <label class="form-label">Email</label>
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Informe o email"> 
+                    <input type="hidden" name="id" id="id" class="form-control" 
+                     value="'.$dados['id'].'"> 
+                    <input type="email" name="email" id="email" class="form-control" 
+                    placeholder="Informe o email" value="'.$dados['email'].'"> 
                 </div>
 
                 <div class="col-sm-3">
                     <label for="" class="form-label">Estado</label>
-                    <select name="estado" id="estado" class="form-control">
-                        <option value="">Estado</option>
+                    <select name="estado" id="estado" class="form-control value="'.$dados['estado'].'"">
+                        <option value="'.$dados['nome'].'">'.$dados['estado'].'</option>
                         <option value="Minas Gerais">Minas Gerais</option>
                         <option value="São Paulo">São Paulo</option>
                         <option value="Mato Grosso">Mato Grosso</option>
@@ -38,24 +44,39 @@ if (isset($_POST['id'])) {
 
                 <div class="col-sm-3">
                     <label for="" class="form-label">CPF</label>
-                    <input type="text" name="cpf" id="" placeholder="CPF" class="form-control">
+                    <input type="text" name="cpf" id="" placeholder="CPF" class="form-control" 
+                     value="'.$dados['cpf'].'">
                 </div>
 
             </div>
                 <div class="row pt-4">
                     <div class="">
-                        <input type="submit" value="Enviar" id="enviar" class="btn btn-primary col-sm-12">
+                        <input type="submit" value="Atualizar" id="update" class="btn btn-primary col-sm-12">
                     </div>
                 </div>
 
         </form>
         ';
-} else {
-    $sql = 'SELECT * FROM user';
+}elseif(isset($_POST['del'])){
+ 
+    $id = $_POST['del'];
+    $sql = 'DELETE FROM user WHERE id = :id';
     $cmd = $pdo->prepare($sql);
-    $cmd->execute();
-    $dados = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    $cmd->bindValue(":id", $id);
+    $dados = $cmd->execute();
 
+    if ($cmd->rowCount() >= 1) {
+        $base .= "<p class='alert alert-success text-center mt-3'> Exclusão realizada!</p>";
+    } else {
+        $base .= "<p class='alert alert-danger text-center mt-3'> Falha na tentativa de remoção! </p>";     
+    }
+
+}else{
+        $sql = 'SELECT * FROM user';
+        $cmd = $pdo->prepare($sql);
+        $cmd->execute();
+        $dados = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    
         foreach ($dados as $dado) 
         {
             $base .= '
@@ -68,11 +89,11 @@ if (isset($_POST['id'])) {
                         <input type="submit" value="Editar" class="edit btn btn-sm 
                             btn-success m-1" id="'.$dado['id'].'">
                         <input type="submit" value="Exlcuir" class="btn btn-sm 
-                            btn-danger mr-1" id="'.$dado['id'].'">
+                            btn-danger mr-1 delete" id="'.$dado['id'].'">
                     </td>
                 </tr>
             ';     
         }
-    }
+}
 
 echo $base;
